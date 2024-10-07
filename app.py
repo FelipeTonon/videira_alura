ba9705a37489e0b75c8c6acdd1ac13b3ddff13e2
FelipeTonon <76598559+FelipeTonon@users.noreply.github.com>
2024-10-07 13:02:01 +0000

    rrr

--- app.py
+++ app.py
@@ -1,26 +1,87 @@
-def previsao(modelo, image):
-    if modelo is None:
-        st.error("Modelo não carregado. Não é possível fazer a previsão.")
-        return
+import streamlit as st
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+import io
+import gdown
+import plotly.express as px
+
+
+@st.cache_resource
+def carrega_modelo():
+    url = 'https://drive.google.com/uc?id=1AYuryrifPeC6BNcwYnRQmjKc37mkJXNX'
     
-    # Make prediction
-    output_data = modelo.predict(image)
+    gdown.download(url,'modelo_quantizado16bits.tflite')
+    interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
+    interpreter.allocate_tensors()
+
     
-    # Apply softmax if needed
-    if output_data.shape[1] > 1:
-        output_data = tf.nn.softmax(output_data[0]).numpy()
-    else:
-        output_data = [1 - output_data[0][0], output_data[0][0]]  # Binary classification adjustment
-    
-    # Define classes and create visualization
+    return interpreter
+
+
+def carrega_imagem():
+    # Cria um file uploader que permite o usuário carregar imagens
+    uploaded_file = st.file_uploader("Arraste e solte uma imagem aqui ou clique para selecionar uma", type=['png', 'jpg', 'jpeg'])
+    if uploaded_file is not None:
+        # Para ler a imagem como um objeto PIL Image
+        image_data = uploaded_file.read()
+        image = Image.open(io.BytesIO(image_data))
+
+        # Mostrar a imagem carregada
+        st.image(image)
+        st.success("Imagem carregada com sucesso!")
+
+        #Pré-processamento da imagem
+        image = np.array(image, dtype=np.float32)
+        image = image / 255.0  # Normalização para o intervalo [0, 1]
+        image = np.expand_dims(image, axis=0)
+
+        return image
+
+
+
+
+def previsao(interpreter,image):
+    # Obtém detalhes dos tensores de entrada e saída
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Define o tensor de entrada para o modelo
+    interpreter.set_tensor(input_details[0]['index'], image)
+
+    # Executa a inferência
+    interpreter.invoke()
+
+    # Obtém a saída do modelo
+    output_data = interpreter.get_tensor(output_details[0]['index'])
     classes = ['Immature', 'Mature']
-    df = pd.DataFrame({
-        'classes': classes,
-        'probabilidades (%)': 100 * np.array(output_data)
-    })
-    
-    # Create bar chart with Plotly
+    df = pd.DataFrame()
+    df['classes'] = classes
+    df['probabilidades (%)'] = 100*output_data[0]
     fig = px.bar(df, y='classes', x='probabilidades (%)', orientation='h', text='probabilidades (%)',
-                 title='Probabilidade de Classes de Catarata')
-    
+             title='Probabilidade de Classes de Doenças em Uvas')
     st.plotly_chart(fig)
+
+#
+def main():
+    st.set_page_config(
+        page_title="Classifica Folhas de Videira",
+        page_icon="🍇",
+    )
+    
+    st.write("# Classifica Folhas de Videira! 🍇")
+    
+
+    interpreter = carrega_modelo()
+
+    image = carrega_imagem()
+
+    if image is not None:
+
+        previsao(interpreter,image)
+    
+
+
+if __name__ == "__main__":
+    main()
