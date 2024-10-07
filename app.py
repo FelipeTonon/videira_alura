abc88a0fe57ea3293748ea1e57f03d58d5abca8a
FelipeTonon <76598559+FelipeTonon@users.noreply.github.com>
2024-10-07 13:55:31 +0000

    ultimo

--- app.py
+++ app.py
@@ -7,31 +7,38 @@ import io
 import gdown
 import plotly.express as px
 
+
 @st.cache_resource
 def carrega_modelo():
     url = 'https://drive.google.com/uc?id=1AYuryrifPeC6BNcwYnRQmjKc37mkJXNX'
-    gdown.download(url, 'modelo_binario.tflite', quiet=False)
-    interpreter = tf.lite.Interpreter(model_path='modelo_binario.tflite')
+    
+    gdown.download(url,'modelo_quantizado16bits.tflite')
+    interpreter = tf.lite.Interpreter(model_path='modelo_quantizado16bits.tflite')
     interpreter.allocate_tensors()
+
+    
     return interpreter
 
+
 def carrega_imagem():
-    uploaded_file = st.file_uploader("Arraste e solte uma imagem aqui ou clique para selecionar uma", type=['png', 'jpg', 'jpeg'])
+    # Cria um file uploader que permite o usuário carregar imagens
+    uploaded_file = st.file_uploader("Arraste e solte uma imagem aqui ou clique para selecionar uma:", type=['png', 'jpg', 'jpeg'])
     if uploaded_file is not None:
+        # Para ler a imagem como um objeto PIL Image
         image_data = uploaded_file.read()
-        image = Image.open(io.BytesIO(image_data)).convert('RGB')
+        image = Image.open(io.BytesIO(image_data))
 
         # Mostrar a imagem carregada
         st.image(image)
         st.success("Imagem carregada com sucesso!")
 
-        # Pré-processamento da imagem
-        image = image.resize((224, 224))  # Ajuste para o tamanho de entrada do seu modelo
+        #Pré-processamento da imagem
         image = np.array(image, dtype=np.float32)
         image = image / 255.0  # Normalização para o intervalo [0, 1]
         image = np.expand_dims(image, axis=0)
 
         return image
+
 
 def previsao(interpreter, image):
     # Obtém detalhes dos tensores de entrada e saída
@@ -44,42 +51,43 @@ def previsao(interpreter, image):
     # Executa a inferência
     interpreter.invoke()
 
-    # Obtém a saída do modelo
+    # Obtém a saída do modelo (probabilidade da classe "Mature")
     output_data = interpreter.get_tensor(output_details[0]['index'])
+    
+    # A probabilidade da classe "Mature"
+    prob_immature = output_data[0][0]
 
-    # Verifica o formato da saída e processa adequadamente
-    if output_data.shape == (1, 1):
-        probabilidade = output_data[0][0]
-        classes = ['Saudável', 'Doente']
-        probabilidades = [100 * (1 - probabilidade), 100 * probabilidade]
-    elif output_data.shape == (1, 2):
-        probabilidades = 100 * output_data[0]
-        classes = ['Saudável', 'Doente']
+    # Exibe a probabilidade para análise
+    st.write(f"Probabilidade de Mature: {prob_immature * 100:.2f}%")
+
+    # Decisão baseada na probabilidade
+    if prob_immature >= 0.5:
+        classe_predita = "Immature"
     else:
-        st.error("Formato de saída do modelo não suportado.")
-        return
+        classe_predita = "Mature"
 
-    df = pd.DataFrame({
-        'classes': classes,
-        'probabilidades (%)': probabilidades
-    })
-    fig = px.bar(df, y='classes', x='probabilidades (%)', orientation='h', text='probabilidades (%)',
-                 title='Probabilidade de Classes')
-    st.plotly_chart(fig)
+    # Exibe o resultado
+    st.success(f"A classe predita é: {classe_predita}")
+
 
 def main():
     st.set_page_config(
-        page_title="Classifica Folhas de Videira",
-        page_icon="🍇",
+        page_title="Classifica Nível de Catarata! 👁️",
+        page_icon="👁️",
     )
     
-    st.write("# Classifica Folhas de Videira! 🍇")
+    st.write("# Classifica Nível de Catarata! 👁️")
     
+
     interpreter = carrega_modelo()
+
     image = carrega_imagem()
 
     if image is not None:
-        previsao(interpreter, image)
+
+        previsao(interpreter,image)
+    
+
 
 if __name__ == "__main__":
     main()
